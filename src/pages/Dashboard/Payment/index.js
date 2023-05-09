fcde699b96b13f9b248ecced85c99108e349e60a
Vini <vinicius.brandaot@gmail.com>
2023-05-09 16:06:24 -0300

    feat: added ticket container

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,29 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import instance from '../../../services/api';
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const [color, setColor] = useState('#FFEED2');
+
+  return (
+    <>
+      <Container backgroundColor={color}>
+        <TextContainer>'Pagamento: Em brevers!'</TextContainer>
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  box-sizing: border-box;
+  background-color: ${(props) => props.backgroundColor};
+
+  width: 145px;
+  height: 145px;
+  left: 341px;
+  top: 323px;
+
+  border: 1px solid #cecece;
+  border-radius: 20px;
+`;
+
+const TextContainer = styled.div``;
