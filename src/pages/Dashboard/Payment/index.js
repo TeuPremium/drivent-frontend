6b4b013327f88733a38f78bfad5c4a6256551f48
Vini <vinicius.brandaot@gmail.com>
2023-05-10 20:44:30 -0300

    removed unused import

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,5 +1,5 @@
 import SelectTicketType from '../../../components/payment/selectTicketType';
-import instance from '../../../services/api';
+
 export default function Payment() {
   return <SelectTicketType />;
 }
