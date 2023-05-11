414e7c72c8b5a7e330b7dfab9cc111def4de0eca
Kevin Alves <kevin.alves.dev@gmail.com>
2023-05-10 23:20:40 -0300

    feature: add pages for cases with no access to the Hotels section + fix: useTicket and useTickets

--- src/hooks/api/useTicket.js
+++ src/hooks/api/useTicket.js
@@ -3,7 +3,7 @@ import useAsync from '../useAsync';
 import * as ticketApi from '../../services/ticketApi';
 import useToken from '../useToken';
 
-export default async function useTicket() {
+export default function useTicket() {
   const token = useToken();
   const { data: ticket, loading: ticketLoading, error: ticketError } = useAsync(() => ticketApi.getTicket(token));
 
